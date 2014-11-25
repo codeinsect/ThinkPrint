0ce752331785e748e95cfb5b16d2711b06a9173f
codeinsect <codeinsect@126.com>
2014-11-25 14:44:43 +0800

    2014-11-25-2

---
+++ ThinkPrint/TP.Repository/ISysSettingRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.Core.Repository;
+using TP.EntityFramework.Models;
+
+namespace TP.Repository
+{
+    public interface ISysSettingRepository : IRepository<SYS_SysSetting>
+    {
+    }
+}
---
+++ ThinkPrint/TP.Repository/SysSettingRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP.EntityFramework;
+using TP.EntityFramework.Models;
+using TP.Repository.Base;
+
+namespace TP.Repository
+{
+    public class SysSettingRepository : BaseRepository<SYS_SysSetting>, ISysSettingRepository
+    {
+        public SysSettingRepository(IDataBaseFactory dataBaseFactory)
+            : base(dataBaseFactory)
+        {
+
+        }
+    }
+}
